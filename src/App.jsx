c11925b388fd521a1e52b4a1d508f9b5ac98bd15
kevinMateus <kevinsmateus.outlook.com>
2024-05-03 12:02:28 +0100

    demo1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts.jsx";
 import Examples from "./components/TabButtons/Examples.jsx";
 
